bbbede7e92b2da8e9c4b61be98a924813c0b0c71
Kevin Herr <kherr@gcmlp.com>
2017-11-15 13:07:39 -0600

    stub web api

---
+++ src/SimpleUptime.WebApi/Controllers/HttpMonitorsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleUptime.WebApi.Controllers
+{
+    [Route("api/httpmonitors")]
+    public class HttpMonitorsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet, Route("{id}")]
+        public IActionResult Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public IActionResult Post()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost, Route("{id}")]
+        public IActionResult Put(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete, Route("{id}")]
+        public IActionResult Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
