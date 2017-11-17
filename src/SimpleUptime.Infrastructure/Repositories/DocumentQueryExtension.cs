2de7a8d8b3e13ff67f96cd9115ccb8e3ed110638
Kevin Herr <kherr9@gmail.com>
2017-11-17 08:52:12 -0600

    stub documentdb

---
+++ src/SimpleUptime.Infrastructure/Repositories/DocumentQueryExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents.Linq;
+
+namespace SimpleUptime.Infrastructure.Repositories
+{
+    internal static class DocumentQueryExtension
+    {
+        public static async Task<T> FirstOrDefaultAsync<T>(this IDocumentQuery<T> query)
+        {
+            if (query.HasMoreResults)
+            {
+                return (await query.ExecuteNextAsync<T>()).FirstOrDefault();
+            }
+
+            return default(T);
+        }
+
+        public static async Task<IEnumerable<T>> ToEnumerableAsync<T>(this IDocumentQuery<T> query)
+        {
+            var result = new List<T>();
+
+            while (query.HasMoreResults)
+            {
+                var batch = await query.ExecuteNextAsync<T>();
+
+                result.AddRange(batch);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/SimpleUptime.Infrastructure/Repositories/HttpMonitorDocumentRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using SimpleUptime.Domain.Models;
+using SimpleUptime.Domain.Repositories;
+
+namespace SimpleUptime.Infrastructure.Repositories
+{
+    public class HttpMonitorDocumentRepository : IHttpMonitorRepository
+    {
+        private const string DatabaseId = "SimpleUptimeDb";
+        private const string CollectionId = "Entities";
+        private readonly IDocumentClient _client;
+
+        public HttpMonitorDocumentRepository(IDocumentClient client)
+        {
+            _client = client;
+        }
+
+        public Task<IEnumerable<HttpMonitor>> GetAsync()
+        {
+            var uri = UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId);
+            var options = new FeedOptions()
+            {
+                PartitionKey = new PartitionKey(nameof(HttpMonitor))
+            };
+
+            var query = _client.CreateDocumentQuery<HttpMonitor>(uri, options)
+                .AsDocumentQuery();
+
+            return query.ToEnumerableAsync();
+        }
+
+        public Task<HttpMonitor> GetByIdAsync(HttpMonitorId id)
+        {
+            var uri = UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId);
+            var options = new FeedOptions()
+            {
+                PartitionKey = new PartitionKey(nameof(HttpMonitor)),
+                MaxItemCount = 1
+            };
+
+            var query = _client.CreateDocumentQuery<HttpMonitor>(uri, options)
+                .Where(x => x.Id == id)
+                .AsDocumentQuery();
+
+            return query.FirstOrDefaultAsync();
+        }
+
+        public Task PutAsync(HttpMonitor httpMonitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(HttpMonitorId id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
