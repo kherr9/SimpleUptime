919c6c502875b43cf6bde9a54014385c8f37e7a9
Kevin Herr <kherr9@gmail.com>
2017-11-24 08:00:41 -0600

    setup worker porj

---
+++ src/SimpleUptime.WorkerApp/Functions.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.ServiceBus.Messaging;
+
+namespace SimpleUptime.WorkerApp
+{
+    public static class Functions
+    {
+        [FunctionName("HandleCheckHttpMonitorPublisher")]
+        public static Task Run(
+            [ServiceBusTrigger("master.events", "worker", AccessRights.Listen, Connection = "azureServiceBus")] string data,
+            TraceWriter log)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
