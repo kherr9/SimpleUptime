b8163fa28c7d56405862721b9769b9d68ec7b666
Kevin Herr <kherr9@gmail.com>
2017-11-28 17:57:53 -0600

    run func app in integration test

---
+++ src/SimpleUptime.IntegrationTests/FuncApp/EndToEndTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SimpleUptime.Domain.Models;
+using SimpleUptime.Domain.Repositories;
+using SimpleUptime.IntegrationTests.Fixtures;
+using Xunit;
+
+namespace SimpleUptime.IntegrationTests.FuncApp
+{
+    public class EndToEndTests : IClassFixture<FuncAppFixture>
+    {
+        private readonly FuncAppFixture _fixture;
+        private readonly IHttpMonitorRepository _httpMonitorRepository;
+        private readonly OpenHttpServer _openHttpServer;
+
+        public EndToEndTests(FuncAppFixture fixture)
+        {
+            _fixture = fixture;
+            _httpMonitorRepository = fixture.HttpMonitorRepository;
+            _openHttpServer = fixture.OpenHttpServer;
+        }
+
+        [Fact]
+        public async Task TestLaunch()
+        {
+            // Arrange
+            var httpMonitor1 = new HttpMonitor(
+                HttpMonitorId.Create(),
+                new HttpRequest()
+                {
+                    Method = HttpMethod.Get,
+                    Url = _fixture.OpenHttpServer.BaseAddress
+                });
+
+            var httpMonitor2 = new HttpMonitor(
+                HttpMonitorId.Create(),
+                new HttpRequest()
+                {
+                    Method = HttpMethod.Delete,
+                    Url = _fixture.OpenHttpServer.BaseAddress
+                });
+
+            await _httpMonitorRepository.PutAsync(httpMonitor1);
+            await _httpMonitorRepository.PutAsync(httpMonitor2);
+
+            var tcs1 = new TaskCompletionSource<object>();
+            var tcs2 = new TaskCompletionSource<object>();
+            var combinedTasks = Task.WhenAll(tcs1.Task, tcs2.Task);
+            _openHttpServer.Handler = ctx =>
+            {
+                if (string.Equals(ctx.Request.Method, httpMonitor1.Request.Method.Method,
+                    StringComparison.InvariantCultureIgnoreCase))
+                {
+                    tcs1.SetResult(null);
+                    ctx.Response.StatusCode = 200;
+                }
+                else if (string.Equals(ctx.Request.Method, httpMonitor2.Request.Method.Method,
+                    StringComparison.InvariantCultureIgnoreCase))
+                {
+                    tcs2.SetResult(null);
+                    ctx.Response.StatusCode = 300;
+                }
+                else
+                {
+                    ctx.Response.StatusCode = 404;
+                }
+
+                return Task.CompletedTask;
+            };
+
+            // Act
+            _fixture.StartHost();
+
+            await Task.WhenAny(combinedTasks, Task.Delay(30000));
+
+            // Assert
+            Assert.True(combinedTasks.IsCompletedSuccessfully);
+        }
+    }
+}
---
+++ src/SimpleUptime.IntegrationTests/FuncApp/FuncAppFixture.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Azure.Documents;
+using SimpleUptime.Domain.Repositories;
+using SimpleUptime.Infrastructure.Repositories;
+using SimpleUptime.IntegrationTests.Fixtures;
+
+namespace SimpleUptime.IntegrationTests.FuncApp
+{
+    public class FuncAppFixture : IDisposable
+    {
+        private Process _process;
+        private readonly DocumentDbFixture _documentDbFixture;
+        private readonly OpenHttpServer _httpServer;
+
+        public FuncAppFixture()
+        {
+            _documentDbFixture = new DocumentDbFixture();
+            _httpServer = OpenHttpServer.CreateAndRun();
+            // todo: delete container
+        }
+
+        public IDocumentClient DocumentClient => _documentDbFixture.DocumentClient;
+
+        public IHttpMonitorRepository HttpMonitorRepository => new HttpMonitorDocumentRepository(DocumentClient, DatabaseConfigurations.Create());
+
+        public IHttpMonitorCheckRepository HttpMonitorCheckRepository => new HttpMonitorCheckDocumentRepository(DocumentClient, DatabaseConfigurations.Create());
+
+        public OpenHttpServer OpenHttpServer => _httpServer;
+
+        public void StartHost()
+        {
+            if (_process != null)
+            {
+                throw new InvalidOperationException("Process already started");
+            }
+
+            var fileName = @"C:\Users\kherr\AppData\Local\Azure.Functions.Cli\1.0.7\func.exe";
+            var args = "host start";
+
+#if DEBUG
+            var workingDirectory = "Debug";
+#else
+            var workingDirectory = "Release";
+#endif
+            _process = new Process
+            {
+                StartInfo =
+                {
+                    UseShellExecute = true,
+                    FileName = fileName,
+                    Arguments = args,
+                    WorkingDirectory = @"C:\git\SimpleUptime\src\SimpleUptime.FuncApp\bin\"+ workingDirectory + @"\net461"
+                }
+            };
+
+            if (!_process.Start())
+            {
+                throw new Exception("Start process returned error");
+            }
+        }
+
+        public void Dispose()
+        {
+            _process?.CloseMainWindow();
+            _process?.Close();
+            _process?.Dispose();
+            _documentDbFixture?.Dispose();
+            _httpServer?.Dispose();
+        }
+    }
+}
