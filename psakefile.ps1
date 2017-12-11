ae8deb039a1b0e6c39cde12dcf47519bfb9cf2b4
Kevin Herr <kherr9@gmail.com>
2017-12-11 13:06:01 -0600

    deploy azure function app

--- psakefile.ps1
+++ psakefile.ps1
@@ -5,7 +5,7 @@ properties {
 
 task default -depends Compile
 
-task Complete -depends Clean, Compile, UnitTests, IntegrationTests 
+task Complete -depends Clean, Compile, UnitTests, IntegrationTests, Pack, Publish
 
 task Clean {
     exec {
@@ -39,6 +39,13 @@ Task Pack {
 
     Remove-Item $destination -Force -Recurse -ErrorAction Ignore
     Copy-Item -Path $source -Recurse -Destination $destination -Force -Container
+
+    "copy azure function"
+    exec {
+        dotnet publish .\src\SimpleUptime.FuncApp -o C:\git\SimpleUptime\artifacts\SimpleUptime.FuncApp --no-restore
+    }
+
+    ZipAzureFunction C:\git\SimpleUptime\artifacts\SimpleUptime.FuncApp C:\git\SimpleUptime\artifacts\SimpleUptime.FuncApp.zip
 }
 
 Task Authenticate {
@@ -50,4 +57,58 @@ Task Authenticate {
         Login-AzureRmAccount
         Save-AzureRmProfile -Path $profile
     }
+}
+
+Task Publish -depends Authenticate {
+    $resourceGroupLocation = "northcentralus"
+    $resourceGroupName = "simpleuptime-uat-rg"
+    $templateFile = "DocumentDB.json"
+    $templateParametersFile = "DocumentDB.parameters.json"
+
+    .\artifacts\SimpleUptime.ResourceGroup\Deploy-AzureResourceGroup.ps1 `
+        -ResourceGroupLocation $resourceGroupLocation `
+        -ResourceGroupName $resourceGroupName `
+        -TemplateFile $templateFile `
+        -TemplateParametersFile $templateParametersFile
+        #-UploadArtifacts `
+        #-ArtifactStagingDirectory "C:\git\SimpleUptime\artifacts\SimpleUptime.ResourceGroup\staging"
+
+    # https://markheath.net/post/deploy-azure-functions-kudu-powershell
+    $resourceGroup = $resourceGroupName
+    $functionAppName = "simpleuptime-uat-func"
+    $creds = Invoke-AzureRmResourceAction -ResourceGroupName $resourceGroup -ResourceType Microsoft.Web/sites/config `
+                -ResourceName $functionAppName/publishingcredentials -Action list -ApiVersion 2015-08-01 -Force
+    
+    $username = $creds.Properties.PublishingUserName
+    $password = $creds.Properties.PublishingPassword
+
+    DeployAzureFunction $username $password $functionAppName "C:\git\SimpleUptime\artifacts\SimpleUptime.FuncApp.zip"
+}
+
+Function ZipAzureFunction(
+    [Parameter(Mandatory = $true)]
+    [String]$functionPath,
+    [Parameter(Mandatory = $true)]
+    [String]$outputPath
+)
+{
+  $excluded = @(".vscode", ".gitignore", "appsettings.json", "secrets")
+  $include = Get-ChildItem $functionPath -Exclude $excluded
+  Compress-Archive -Path $include -Update -DestinationPath $outputPath
+}
+
+Function DeployAzureFunction(
+    [Parameter(Mandatory = $true)]
+    [String]$username,
+    [Parameter(Mandatory = $true)]
+    [String]$password,
+    [Parameter(Mandatory = $true)]
+    [String]$functionAppName,
+    [Parameter(Mandatory = $true)]
+    [String]$zipFilePath    
+)
+{
+  $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
+  $apiUrl = "https://$functionAppName.scm.azurewebsites.net/api/zip/site/wwwroot"
+  Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method PUT -InFile $zipFilePath -ContentType "multipart/form-data"
 }
