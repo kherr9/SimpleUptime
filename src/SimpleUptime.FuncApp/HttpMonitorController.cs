98aa2d1e8ba9ddb6d0089f72169e6cb3ca09a1de
Kevin Herr <kherr9@gmail.com>
2017-12-11 16:17:31 -0600

    stub get all

---
+++ src/SimpleUptime.FuncApp/HttpMonitorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using SimpleUptime.Application.Services;
+using SimpleUptime.FuncApp.Infrastructure;
+
+namespace SimpleUptime.FuncApp
+{
+    public static class HttpMonitorController
+    {
+        [FunctionName("HttpMonitorsGet")]
+        public static HttpResponseMessage GetAsync(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "httpmonitors")]HttpRequestMessage req,
+            TraceWriter log)
+        {
+            return req.CreateResponse(HttpStatusCode.OK, "Success me");
+        }
+    }
+}
