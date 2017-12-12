ccfb9c864d4eefe1442009709cbf47f7853c1b29
Kevin Herr <kherr9@gmail.com>
2017-12-12 10:29:11 -0600

    add delete

--- src/SimpleUptime.FuncApp/HttpMonitorController.cs
+++ src/SimpleUptime.FuncApp/HttpMonitorController.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Threading.Tasks;
@@ -6,7 +7,9 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using SimpleUptime.Application.Commands;
+using SimpleUptime.Application.Exceptions;
 using SimpleUptime.Application.Services;
+using SimpleUptime.Domain.Models;
 using SimpleUptime.FuncApp.Infrastructure;
 
 namespace SimpleUptime.FuncApp
@@ -37,6 +40,40 @@ namespace SimpleUptime.FuncApp
             var httpMonitor = await service.CreateHttpMonitorAsync(cmd);
 
             return req.CreateResponse(HttpStatusCode.OK, httpMonitor, formatter);
+        }
+
+        [FunctionName("HttpMonitorsDelete")]
+        public static async Task<HttpResponseMessage> DeleteAsync(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "httpmonitors/{httpMonitorId}")]HttpRequestMessage req,
+            string httpMonitorId,
+            TraceWriter log,
+            [Inject] IHttpMonitorService service,
+            [Inject] JsonMediaTypeFormatter formatter)
+        {
+            HttpMonitorId id;
+            try
+            {
+                id = new HttpMonitorId(Guid.Parse(httpMonitorId));
+            }
+            catch (FormatException)
+            {
+                return req.CreateResponse(HttpStatusCode.NotFound);
+            }
+            catch (ArgumentException)
+            {
+                return req.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            try
+            {
+                await service.DeleteHttpMonitorAsync(id);
+
+                return req.CreateResponse(HttpStatusCode.NoContent);
+            }
+            catch (EntityNotFoundException)
+            {
+                return req.CreateResponse(HttpStatusCode.NotFound);
+            }
         }
     }
 }
