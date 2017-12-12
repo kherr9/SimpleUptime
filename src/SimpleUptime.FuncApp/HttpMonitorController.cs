dc8c3eda2abdc2c3aa3f187cb742828049011a8f
Kevin Herr <kherr9@gmail.com>
2017-12-12 09:14:19 -0600

    add app

--- src/SimpleUptime.FuncApp/HttpMonitorController.cs
+++ src/SimpleUptime.FuncApp/HttpMonitorController.cs
@@ -1,10 +1,14 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using SimpleUptime.Application.Commands;
 using SimpleUptime.Application.Services;
 using SimpleUptime.FuncApp.Infrastructure;
 
@@ -14,13 +18,27 @@ namespace SimpleUptime.FuncApp
     {
         [FunctionName("HttpMonitorsGet")]
         public static async Task<HttpResponseMessage> GetAsync(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "httpmonitors")]HttpRequestMessage req,
-            TraceWriter log,
-            [Inject] IHttpMonitorService services)
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "httpmonitors")]HttpRequestMessage req,
+        TraceWriter log,
+        [Inject] IHttpMonitorService service)
         {
-            var httpMonitors = await services.GetHttpMonitorsAsync();
+            var httpMonitors = await service.GetHttpMonitorsAsync();
 
             return req.CreateResponse(HttpStatusCode.OK, httpMonitors, MediaTypeHeaderValue.Parse("application/json"));
+        }
+        
+        [FunctionName("HttpMonitorsPost")]
+        public static async Task<HttpResponseMessage> PostAsync(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "httpmonitors")]HttpRequestMessage req,
+            TraceWriter log,
+            [Inject] IHttpMonitorService service)
+        {
+            var json = await req.Content.ReadAsStringAsync();
+            var cmd = JsonConvert.DeserializeObject<CreateHttpMonitor>(json);
+
+            var httpMonitor = await service.CreateHttpMonitorAsync(cmd);
+
+            return req.CreateResponse(HttpStatusCode.OK, httpMonitor, MediaTypeHeaderValue.Parse("application.json"));
         }
     }
 }
