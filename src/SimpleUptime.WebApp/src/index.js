1c515e897d8a55c59d91246673c08e03d0037109
Kevin Herr <kherr9@gmail.com>
2017-12-21 07:49:13 -0600

    remove old commented out code

--- src/SimpleUptime.WebApp/src/index.js
+++ src/SimpleUptime.WebApp/src/index.js
@@ -2,17 +2,10 @@ import _ from 'lodash';
 import $ from 'jquery-slim';
 import Navigo from 'navigo'; 
 import printMe from './print.js';
-//import './styles.css';
 import { cube } from './math.js';
 import CheckService from './CheckService';
 import AddCheckViewModel from './AddCheckViewModel';
 import CheckListViewModel from './CheckListViewModel';
-
-/*
-if(process.env.NODE_ENV !== 'production'){
-  console.log('Looks like we are in development mode!')
-}
-*/
 
 var checkService = null;
 console.log(window.location.hostname);
@@ -70,32 +63,3 @@ $(function () {
     return o;
   };
 })($);
-
-/*
-function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpack', cube(5)], ' ');
-
-  btn.innerHTML = 'Click me and check the console';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
-}
-
-let element = component(); // Store the element to re-render on print.js changes
-document.body.appendChild(element);
-
-if (module.hot) {
-  module.hot.accept('./print.js', function () {
-    console.log('Accepting the updated printMe module!');
-    printMe();
-    document.body.removeChild(element);
-    element = component(); // Re-render the "component" to update the click handler
-    document.body.appendChild(element);
-  })
-}
-*/
